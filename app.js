591b648355d24da1369668d2e6d8a0d6a5522160
DJshelfmushroom <67295452+DJshelfmushroom@users.noreply.github.com>
2024-01-17 05:38:24 +0000

    abcdefghijklmnopqrstuvwxyz

--- app.js
+++ app.js
@@ -9,13 +9,10 @@ const app = new App({
 const net = require('net');
 const server = net.createServer(socket => {
   socket.on('data', data => {
-    if (data.toString() === 'PING') {
       socket.write('HTTP/1.1 200 OK\r\n\r\n');
       socket.end();
-    }
   });
 });
-
 server.listen(3000, 'localhost');
 (async () => {
     const port = 3000;
